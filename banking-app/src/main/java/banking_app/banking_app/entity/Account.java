1618d6ec89e6b12af4159bd7a616d1a488ca36e1
vikram17dp <vikramdp505@gmail.com>
2025-05-01 18:54:25 +0530

    creating an account classes for banking app

---
+++ banking-app/src/main/java/banking_app/banking_app/entity/Account.java
@@ -0,0 +1,23 @@
+package banking_app.banking_app.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="accounts")
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// it will increment the primary key
+    private Long id;
+    @Column(name = "account_holder_name")
+    private String accountHolderName;
+    private double balance;
+}
