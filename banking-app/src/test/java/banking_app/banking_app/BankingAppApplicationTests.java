11a5ec2fcc107b78ea519847b5debb8aff790e2a
vikram17dp <vikramdp505@gmail.com>
2025-05-01 18:27:02 +0530

    creating an banking_app with crud operations

---
+++ banking-app/src/test/java/banking_app/banking_app/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package banking_app.banking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
